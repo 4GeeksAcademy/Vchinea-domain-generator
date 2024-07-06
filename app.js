219fca4ff9ea0ec2f2a8b46f9649a04c90e3987c
Victor Chinea <77308527+nobystanders@users.noreply.github.com>
2024-07-06 00:21:54 +0000

    done

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let pronoun = ['the', 'our'];
-    let adj = ['great', 'big'];
-    let noun = ['jogger', 'racoon'];
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
 
-    function generateDomains(pronoun, adj, noun) {
-        let domains = [];
-        for (let p of pronoun) {
-            for (let a of adj) {
-                for (let n of noun) {
-                    domains.push(p + a + n + ".com");
-                }
-            }
+  function generateDomains(pronoun, adj, noun) {
+    let domains = [];
+    for (let p of pronoun) {
+      for (let a of adj) {
+        for (let n of noun) {
+          domains.push(p + a + n + ".com");
         }
-        return domains;
+      }
     }
+    return domains;
+  }
 
-    function displayDomains(domains) {
-        const domainList = document.getElementById('domains');
-        domains.forEach(domain => {
-            let li = document.createElement('li');
-            li.textContent = domain;
-            domainList.appendChild(li);
-        });
-    }
+  function displayDomains(domains) {
+    const domainList = document.getElementById("domains");
+    domains.forEach((domain) => {
+      let li = document.createElement("li");
+      li.textContent = domain;
+      domainList.appendChild(li);
+    });
+  }
 
-    let generatedDomains = generateDomains(pronoun, adj, noun);
-    displayDomains(generatedDomains);
+  let generatedDomains = generateDomains(pronoun, adj, noun);
+  displayDomains(generatedDomains);
 });
